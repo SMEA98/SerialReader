d3b03f7014c94738cf6d93c2f7a2d4b5537646f7
unknown <steven.m@easyaerial.com>
2021-12-21 10:19:58 -0500

    reader python file

---
+++ reader.py
@@ -0,0 +1,21 @@
+import serial 
+
+ser = serial.Serial(
+        port = 'COM17',\
+        baudrate = 9600,\
+        parity = serial.PARITY_NONE,\
+        stopbits = serial.STOPBITS_ONE,\
+        bytesize = serial.EIGHTBITS,\
+        timeout =0)
+
+print("Connected to: " + ser.portstr)
+count = 1
+
+while True:
+    line = ser.readline()
+    print(str(count) + str(': ') + str(line) )
+    count += 1
+
+ser.close()
+
+
